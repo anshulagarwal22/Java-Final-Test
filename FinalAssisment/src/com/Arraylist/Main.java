e298d89d375811aa5706a8c2599e949900eb9b8c
anshulagarwal22 <44352578+anshulagarwal22@users.noreply.github.com>
2021-10-11 17:44:25 +0530

    Update Main.java

--- FinalAssisment/src/com/Arraylist/Main.java
+++ FinalAssisment/src/com/Arraylist/Main.java
@@ -1,22 +1,61 @@
-package question1;
 
-import java.util.Scanner;
-public class Main {
+package Arraylist;
 
-	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Give Number of Employees");
-		int n = sc.nextInt();
-		//sc.close();
-		Employee arr[] = new Employee[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = new Employee();
-			arr[i].getData();
-		}
-		for(int i=0; i<n; i++) {
-			arr[i].displayData();
-		}
-	}
+import java.util.*;
+ 
+class Employee{
+    public int id;
+    public String name;
+    public String address;
+    public float sal;
+    public static int count = 0;
+ 
+    //zero argument constructor. Gets called by default when an object is created for the Employee class
+    public Employee(){}
+ 
+    //Parameterized Constructor
+    public Employee(int id, String name,String address,float sal)
+    {
+        super();
+        this.id = id;
+        this.name = name;
+        this.address=address;
+        this.sal= sal;
+        
+        count++;
+    }
+ 
+    public int getId() {
+    return id;
+    }
+ 
+    public String getName() {
+    return name;
+    }
+ 
+    public String getAddress() {
+    return address;
+    }
 
+    public float getSalary() {
+    return sal;
+    }
 }
+ 
+public class DisplayArrayList {
+public static void main(String[] args) throws Exception 
+    {
+     List<Employee> list = new ArrayList<Employee>();
+ 
+     list.add(new Employee(1, "Ravi","Delhi",5000));
+     list.add(new Employee(2, "Raj","Mumbai",7000));
+     list.add(new Employee(3, "Rekha","Chennai",7000));
+     list.add(new Employee(4, "Ram","Rajasthan",6000));
+ 
+        for (Employee s : list) //Iterates as long as there are elements in the list. An object s is created of type Employee class.
+        {
+            System.out.print("ID, Name, Address And salary of the employee are : ");
+            System.out.println(s.getId()+" "+s.getName()+" " +s.getAddress()+" " +s.getSalary());
+        }
+    }//End of main() method
+}//end of DisplayArrayList class
